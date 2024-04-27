e9ca265ff0fe7529ad7055c71c18067141f7a13e
Matthew Kaplan <mlk9120@nyu.edu>
2024-04-26 22:41:05 -0400

    add real time

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import argparse
 import csp
 import os
+
+from csp_mta import (
+    GTFS_DIRECTION,
+    LINE_TO_ENDPOINT,
+    STOP_INFO_DF,
+    GTFSRealtimeInputAdapter,
+    nyct_subway_pb2,
+)
+
 from datetime import datetime, timedelta
 from e_01_nyct_subway import get_stop_time_at_station, filter_trains_headed_for_stop, next_N_trains_at_stop, get_terminus, entities_to_departure_board_str, departure_board;
 
@@ -14,18 +23,7 @@ if __name__ == '__main__':
         help="File which stores the recorded data",
     )
     parser.add_argument(
-        "--line",
-        type=str,
-        default="NULL",
-        required=True,
-        help="line ID, such as ACE"
-    )
-    parser.add_argument(
-        "--stop_id",
-        type=str,
-        default=None,
-        required=True,
-        help="stop_id from data/stops.txt",
+        "platforms", nargs="+", help="stop_id:service pair: see stops.txt for stop_id"
     )
     parser.add_argument(
         "--direction",
@@ -51,8 +49,31 @@ if __name__ == '__main__':
     )
     args = parser.parse_args()
 
-    stop_id = args.stop_id
-    line = args.line
+    #stop_id = args.stop_id
+    #line = args.line
+
+    platforms_to_subscribe_to = []
+    for platform in args.platforms:
+        stop_id, train_line = tuple(platform.split(":"))
+
+        # process which line to track
+        found = False
+        for line in LINE_TO_ENDPOINT.keys():
+            if train_line in line:
+                train_line = line
+                found = True
+                break
+        if not found:
+            raise ValueError(f"Did not recognize service {train_line}")
+
+        # process stop_id
+        if stop_id not in STOP_INFO_DF.index:
+            raise ValueError(
+                f"Did not recognize stop_id {stop_id}: see stops.txt for valid stop_ids"
+            )
+
+        platforms_to_subscribe_to.append((stop_id, train_line))
+    
 
     csp.run(
             departure_board,
